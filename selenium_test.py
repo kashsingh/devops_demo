1f041866ecd0322fe5bc172618c49a74e3679b24
Akash Singh <max.payne61@gmail.com>
2020-06-01 23:11:59 +0530

    demo selenium test

--- selenium_test.py
+++ selenium_test.py
@@ -4,11 +4,16 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
+
 driver.get("http://35.154.147.222/")
+
 assert "Simple PHP Website" in driver.title
 assert driver.find_element_by_id("Home")
 assert driver.find_element_by_id("About Us")
 assert driver.find_element_by_id("Products")
 assert driver.find_element_by_id("Contact")
+
+# Uncomment below line to fail a test
 #assert driver.find_element_by_id("Noe")
+
 driver.close()
